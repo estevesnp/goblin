b2c4ea9e2dec73ecfb140fa4c8de1102d98754c6
estevesnp <nuno.pinto@live.com>
2024-07-08 00:15:32 +0100

    started lexer

---
+++ pkg/token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL TokenType = "ILLEGAL"
+	EOF     TokenType = "EOF"
+
+	// Identifiers + Literals
+	IDENT TokenType = "IDENT"
+	INT   TokenType = "INT"
+
+	// Operators
+	ASSIGN TokenType = "="
+	PLUS   TokenType = "+"
+
+	// Delimiters
+	COMMA     TokenType = ","
+	SEMICOLON TokenType = ";"
+	LPAREN    TokenType = "("
+	RPAREN    TokenType = ")"
+	LBRACE    TokenType = "{"
+	RBRACE    TokenType = "}"
+
+	// Keywords
+	FUNCTION TokenType = "FUNCTION"
+	LET      TokenType = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
